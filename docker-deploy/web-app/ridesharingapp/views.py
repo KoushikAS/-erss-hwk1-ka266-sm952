bd6fff0e5cee50097ea5922bf50e877867259b6e
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-02-05 11:04:52 -0500

    Delete and Edit party done

--- docker-deploy/web-app/ridesharingapp/views.py
+++ docker-deploy/web-app/ridesharingapp/views.py
@@ -140,7 +140,7 @@ def save_driver_db(request, form):
 # Saving Driver details
 def save_driver(request, action):
     if action == 'edit':
-        instance = Driver.objects.get(user=request.user)
+        instance = Driver.objects.get(user_id=request.user.id)
     else:
         instance = None
 
@@ -233,14 +233,14 @@ def create_ride(request):
 # Ride Requesting Editing
 def edit_ride(request, rideId):
     check_user_authentication(request)
-    try:
-        ride = Ride.objects.get(rideId=rideId)
-    except Ride.DoesNotExist:
-        messages.error(request, f"Ride not found!")
-        return redirect('home')
+    check_ride_exists(request, rideId)
+
+    ride = Ride.objects.get(rideId=rideId)
+
     if request.user.id != ride.rideOwner.owner_id:
         messages.error(request, f"Not authorized!")
         return redirect('home')
+
     if ride.isRideEditable():
         data = {
             'source': ride.source,
@@ -306,18 +306,21 @@ def view_ride(request, rideId):
     ride_serialized = RideSerializers(ride, many=False)
     ownerParty = Party.objects.get(id=ride.rideOwner_id)
     parties = []
-    parties.append({'ownerName': User.objects.get(id=ownerParty.owner_id).username ,'passengers': ownerParty.passengers, 'owner': True})
-
+    parties.append({'ownerName': User.objects.get(id=ownerParty.owner_id).username, 'passengers': ownerParty.passengers,
+                    'owner': True})
 
     canConfirmRide = False
     canEdit = False
     canCompleteRide = False
     canJoinRide = False
+    canEditParty = False
+
+    isUserLinked = is_driver_in_ride(request.user.id, ride)
 
     if ownerParty.owner_id == request.user.id and ride.status == Ride.RideStatus.OPEN:
         canEdit = True
 
-    if request.session.get('driverView') and not is_driver_in_ride(request.user.id, ride):
+    if request.session.get('driverView') and not isUserLinked:
 
         if ride.status == Ride.RideStatus.OPEN:
             canConfirmRide = True
@@ -325,7 +328,7 @@ def view_ride(request, rideId):
         if ride.status == Ride.RideStatus.CONFIRMED and Driver.objects.filter(id=ride.driver_id).exists():
             canCompleteRide = True
 
-    if not request.session.get('driverView') and ride.isSharable is True and ride.status == Ride.RideStatus.OPEN and ownerParty.owner_id != request.user.id:
+    if ride.isSharable is True and ride.status == Ride.RideStatus.OPEN and ownerParty.owner_id != request.user.id and not isUserLinked:
         canJoinRide = True
 
     if ride.driver:
@@ -341,15 +344,13 @@ def view_ride(request, rideId):
             {'ownerName': User.objects.get(id=party.owner_id).username, 'passengers': party.passengers,
              'owner': False})
 
+    if isUserLinked and ride.rideOwner.owner_id != request.user.id:
+        canEditParty = True
+
     return render(request, 'view-ride.html',
                   {'ride': ride_serialized.data, 'canEdit': canEdit, 'driver': driver, 'driverName': driverName,
-                   'canConfirmRide': canConfirmRide, 'canCompleteRide': canCompleteRide, 'canJoinRide': canJoinRide, 'parties': parties})
-
-
-
-# Ride Searching Sharer: Similar to Ride Selection but with filters and open rides driver
-def ride_searching_sharer(request):
-    return HttpResponse("Page Under Development")
+                   'canConfirmRide': canConfirmRide, 'canCompleteRide': canCompleteRide, 'canJoinRide': canJoinRide,
+                   'canEditParty': canEditParty, 'parties': parties})
 
 
 # Ride Complete: when driver completes the ride
@@ -408,32 +409,90 @@ def ride_confirmed(request, rideId):
 
 
 # Join a sharable Ride.
-def join_ride(request, rideId):
-    check_user_authentication(request)
-    check_ride_exists(request, rideId)
-
+def party_ride(request, rideId, ride, instance):
     if request.POST:
-        form = PartyForm(request.POST)
-        if form.is_valid():
-            ride = Ride.objects.get(rideId=rideId)
+        form = PartyForm(request.POST, instance=instance)
 
-            if ride.availablePassengers <= form.cleaned_data['passengers']:
-                messages.error(request, f"Party can only accommodate " + str(ride.availablePassengers) + ". Please try "
-                                                                                                         "another ride!")
+        if instance:
+            # If Edit Revert the available list
+            ride.availablePassengers = ride.availablePassengers + instance.passengers
+
+        if form.is_valid():
+            if ride.availablePassengers < form.cleaned_data['passengers']:
+                messages.error(request, f"Party can only accommodate " + str(
+                    ride.availablePassengers) + ". Please try another ride!")
                 return redirect('home')
 
-            party = Party(owner=request.user, passengers=form.cleaned_data['passengers'])
+            # Edit Ride
+            if instance:
+                party = instance
+                party.passengers = form.cleaned_data['passengers']
+            else:
+                party = Party(owner=request.user, passengers=form.cleaned_data['passengers'])
+
             party.save()
             ride.availablePassengers = ride.availablePassengers - form.cleaned_data['passengers']
             ride.rideShared.add(party)
             ride.save()
-            messages.success(request, " Successfully Joined the ride.")
+            if instance:
+                messages.success(request, " Successfully edited the party!")
+            else:
+                messages.success(request, " Successfully Joined the ride.")
             return redirect('home')
         else:
             messages.error(request, f'Invalid Entry')
     else:
-        form = PartyForm()
+        form = PartyForm(instance=instance)
     return render(request, 'party-form.html', {'form': form, 'rideId': rideId})
+
+
+def join_ride(request, rideId):
+    check_user_authentication(request)
+    check_ride_exists(request, rideId)
+    ride = Ride.objects.get(rideId=rideId)
+    return party_ride(request, rideId, ride, None)
+
+
+def editParty(request, rideId):
+    check_user_authentication(request)
+    check_ride_exists(request, rideId)
+    ride = Ride.objects.get(rideId=rideId)
+
+    currentparty = None
+    for party in ride.rideShared.all():
+        if party.owner_id == request.user.id:
+            currentparty = party
+            break
+
+    if not currentparty:
+        messages.error(request, f'You are not part of shared list.')
+        return redirect('home')
+
+    return party_ride(request, rideId, ride, currentparty)
+
+
+def deleteParty(request, rideId):
+    check_user_authentication(request)
+    check_ride_exists(request, rideId)
+    ride = Ride.objects.get(rideId=rideId)
+
+    currentparty = None
+    for party in ride.rideShared.all():
+        if party.owner_id == request.user.id:
+            currentparty = party
+            break
+
+    if not currentparty:
+        messages.error(request, f'You are not part of shared list.')
+        return redirect('home')
+
+    ride.availablePassengers = ride.availablePassengers + currentparty.passengers
+    ride.save()
+    currentparty.delete()
+
+    messages.info(request, f"Successfully deleted the party.")
+    return redirect('home')
+
 
 
 def open_rides_sharer(request):
@@ -454,7 +513,7 @@ def open_rides_sharer(request):
                                             destinationArrivalTimeStamp__gte=form.cleaned_data['earliestArrivalTime'],
                                             destinationArrivalTimeStamp__lte=form.cleaned_data['latestArrivalTime'],
                                             availablePassengers__gte=form.cleaned_data['passengers']) \
-                                    .order_by('destinationArrivalTimeStamp')
+                    .order_by('destinationArrivalTimeStamp')
             else:
                 messages.error(request, f'Invalid Entry')
             canReset = True
